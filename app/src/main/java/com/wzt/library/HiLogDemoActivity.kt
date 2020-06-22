ae05001483d28faaec9bff54d53d0ecf32504df4
wangzitao <wangzi08201003>
2020-06-23 01:23:25 +0800

    function：新增日志可视化功能实现，可在界面上直接观看到日志打印

--- app/src/main/java/com/wzt/library/HiLogDemoActivity.kt
+++ app/src/main/java/com/wzt/library/HiLogDemoActivity.kt
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.wzt.hilog.HiLog
 import com.wzt.hilog.HiLogConfig
+import com.wzt.hilog.HiLogManager
 import com.wzt.hilog.HiLogType
+import com.wzt.hilog.printer.HiViewPrinter
 import kotlinx.android.synthetic.main.activity_hi_log_demo.*
 
 class HiLogDemoActivity : AppCompatActivity() {
+    var viewPrinter :HiViewPrinter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hi_log_demo)
+        viewPrinter = HiViewPrinter(this)
+        HiLogManager.getInstance().addPrinter(viewPrinter)
         btn_log.setOnClickListener {
             printLog()
         }
+        viewPrinter?.viewProvider?.showFloatingView()
     }
 
     private fun printLog() {
